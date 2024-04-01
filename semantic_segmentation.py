cd598f3907cbb148e19388e182379d3814dd4970
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-04-01 18:36:49 +0100

    Update remove_inds function

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -49,30 +49,23 @@ def get_segementation(
     return mmask
 
 
-def remove_inds(input_img: np.ndarray, inds: list, colour: list) -> np.ndarray:
-    """Make a copy of the image and set the indices of everything but the walls to black.
+def remove_inds(width, height, inds) -> np.ndarray:
+    """Create a wall mask by creating a white image and setting the indices of everything but the walls to black.
 
     Parameters
     ----------
-    input_img : np.ndarray
-        The input image of a room interior.
-    other : np.ndarray
-        Indices of non-wall pixels in the input image.
+    !!!
 
     Returns
     -------
     pil.Image
         The input image with everything except the walls set to black.
     """
-    input_copy = input_img.copy()
-    if not isinstance(input_copy, np.ndarray):
-        input_copy = input_copy.asnumpy()
-    input_copy = input_copy.astype(float)
-    for ind_list in inds:
-        input_copy[ind_list[0], ind_list[1]] = np.array(colour, dtype=float)
-    input_copy = np.clip(input_copy, 0, 255)
+    img = np.ones((height, width, 3))
+    img = np.where(img==1, 255, img)
+    img[inds[0], inds[1]] = [0, 0, 0]
 
-    segmented_input = input_copy.astype(np.uint8)
+    segmented_input = img.astype(np.uint8)
     segmented_input_img = pil.fromarray(segmented_input)
     segmented_input_img.save("images/outputs/intermediate-outputs/segmentation-walls-only.png")
 
